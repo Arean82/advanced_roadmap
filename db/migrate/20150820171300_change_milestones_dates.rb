60ea93e5b92e783cf011e8f1f6a69a5592305fbf
arean82 <38061300+Arean82@users.noreply.github.com>
2025-05-15 11:41:48 +0530

    Update 20150820171300_change_milestones_dates.rb

--- db/migrate/20150820171300_change_milestones_dates.rb
+++ db/migrate/20150820171300_change_milestones_dates.rb
@@ -1,9 +1,3 @@
-# Copyright © Emilio González Montaña
-# Licence: Attribution & no derivates
-#   * Attribution to the plugin web page URL should be done if you want to use it.
-#     https://redmine.ociotec.com/projects/redmine-plugin-scrum
-#   * No derivates of this plugin (or partial) are allowed.
-# Take a look to licence.txt file at plugin root folder for further details.
 
 class ChangeMilestonesDates < ActiveRecord::Migration[4.2]
 #class ChangeMilestonesDates < ActiveRecord::Migration
